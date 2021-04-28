c41c674ae6d1c6427c4bc881c530e7c65efea28e
Francisco Alface <falface@gmail.com>
2021-04-28 11:22:02 -0300

    Added python version of the two_sums problem

---
+++ src/top-interview-questions/two-sums/test_two_sums.py
@@ -0,0 +1,14 @@
+import pytest
+from two_sums import two_sums
+
+
+@pytest.mark.parametrize(
+    'nums, target, expected',
+    [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+    ]
+)
+def test_two_sums(nums, target, expected):
+    assert expected == two_sums(nums, target)
