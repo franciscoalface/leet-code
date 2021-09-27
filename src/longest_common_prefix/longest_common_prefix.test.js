f9ba1254e200d35cbabac309170081ac9a131dfe
Francisco Alface <francisco.alface.br@avenuecode.com>
2021-09-27 18:21:57 -0300

    Reorganizing the repo

--- src/longest_common_prefix/longest_common_prefix.test.js
+++
@@ -1,9 +0,0 @@
-const longestCommonPrefix = require("./longest_common_prefix");
-
-test.skip("test example 1 -> ['flower','flow','flight'] === 'fl'", () => {
-	expect(longestCommonPrefix(["flower", "flow", "flight"])).toStrictEqual("fl");
-});
-
-test.skip("test example 2 -> ['dog','racecar','car'] === ''", () => {
-	expect(longestCommonPrefix(["dog", "racecar", "car"])).toStrictEqual("");
-});
