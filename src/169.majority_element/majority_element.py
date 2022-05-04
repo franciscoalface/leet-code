a1ef0f75799e6866f09a758732ec544011a1680c
Francisco Alface <francisco.alface.br@avenuecode.com>
2022-05-04 10:30:44 -0300

    Added a new solution to the challenge

--- src/169.majority_element/majority_element.py
+++ src/169.majority_element/majority_element.py
@@ -1,16 +1,28 @@
+# def majority_element(nums):
+#     m = {}
+#     number = ""
+#     appearances = 0
+#     for num in nums:
+#         if num in m:
+#             m[num] += 1
+#         else:
+#             m[num] = 1
+
+#     for key, value in m.items():
+#         if value > appearances:
+#             number = key
+#             appearances = value
+
+#     return number
+
+
 def majority_element(nums):
     m = {}
-    number = ""
-    appearances = 0
+    size = len(nums)
+
     for num in nums:
-        if num in m:
-            m[num] += 1
-        else:
-            m[num] = 1
+        m[num] = m.get(num, 0) + 1
 
     for key, value in m.items():
-        if value > appearances:
-            number = key
-            appearances = value
-
-    return number
+        if value > size // 2:
+            return key
