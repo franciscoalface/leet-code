54926abd36344aef11de63cb42b9c7578eb385d8
Francisco Alface <francisco.alface.br@avenuecode.com>
2022-05-04 10:07:43 -0300

    Removed assertions from solution file

--- src/217.contains_duplicate/contains_duplicate.py
+++ src/217.contains_duplicate/contains_duplicate.py
@@ -29,8 +29,3 @@ def contains_duplicate(nums):
             return True
         existing_nums.add(num)
     return False
-
-
-assert contains_duplicate([1, 2, 3, 1]) == True
-assert contains_duplicate([1, 2, 3, 4]) == False
-assert contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
